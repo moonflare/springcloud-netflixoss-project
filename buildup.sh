ce695f4434afab4c2ac5ec1c804c3871bfb9642b
Cleverson Ledur <cleverson.ledur@schoolastic-app.com>
2018-01-17 15:06:46 -0200

    fix(Microservice): Fixing dockerfile and script.

--- buildup.sh
+++ buildup.sh
@@ -3,6 +3,7 @@
 cd config-server ; ./mvnw clean install -U ; cd ..
 cd eureka-server ; ./mvnw clean install -U ; cd ..
 cd gateway-server ; ./mvnw clean install -U ; cd ..
+cd microservice ; ./mvnw clean install -U ; cd ..
 
 docker-compose build 
 docker-compose up
