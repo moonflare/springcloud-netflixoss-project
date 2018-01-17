ed1e06b73027fa0f8c564fced3bf39d82e377f60
Cleverson Ledur <cleverson.ledur@schoolastic-app.com>
2018-01-17 14:55:31 -0200

    fix(Dockers): Adjusting projects to use docker.

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-cd config-server ; ./mvnw clean install -X -U ; cd ..
-cd eureka-server ; ./mvnw clean install -X -U ; cd ..
-cd gateway-server ; ./mvnw clean install -X -U ; cd ..
+cd config-server ; ./mvnw clean install -U ; cd ..
+cd eureka-server ; ./mvnw clean install -U ; cd ..
+cd gateway-server ; ./mvnw clean install -U ; cd ..
+cd microservice ; ./mvnw clean install -U ; cd ..
 
 docker-compose build 
