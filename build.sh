5268da42038a25a85680066078e0b900e6ba520e
Cleverson Ledur <cleverson.ledur@schoolastic-app.com>
2018-01-17 15:15:28 -0200

    fix(Docker): Fixing docker scripts.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd config-server ; ./mvnw clean install -U ; cd ..
-cd eureka-server ; ./mvnw clean install -U ; cd ..
-cd gateway-server ; ./mvnw clean install -U ; cd ..
-cd microservice ; ./mvnw clean install -U ; cd ..
+cd config-server ; ./mvnw clean install -U -Dmaven.test.skip=true; cd ..
+cd eureka-server ; ./mvnw clean install -U -Dmaven.test.skip=true; cd ..
+cd gateway-server ; ./mvnw clean install -U -Dmaven.test.skip=true; cd ..
+cd microservice ; ./mvnw clean install -U -Dmaven.test.skip=true; cd ..
 
 docker-compose build 
